008f521c2f0758dd975ebda5756047f0c3e5a9c9
Rob Eberhardt <rob.eberhardt@gmail.com>
2018-10-31 13:42:26 -0400

    end of chapter 23

--- sick-fits/frontend/components/Items.js
+++ sick-fits/frontend/components/Items.js
@@ -4,6 +4,7 @@ import gql from "graphql-tag";
 import styled from "styled-components";
 import Item from "./Item";
 import Pagination from "./Pagination";
+import { perPage } from "../config";
 
 const Center = styled.div`
   text-align: center;
@@ -18,8 +19,11 @@ const ItemsList = styled.div`
 `;
 
 const ALL_ITEMS_QUERY = gql`
-  query ALL_ITEMS_QUERY {
-    items {
+  query ALL_ITEMS_QUERY(
+    $skip: Int = 0,
+    $first: Int = ${perPage}
+  ) {
+    items(first: $first, skip: $skip, orderBy: createdAt_DESC) {
       id
       title
       price
@@ -34,22 +38,29 @@ class Items extends Component {
   render() {
     return (
       <Center>
-        <Pagination page={this.props.page} />
-
-        <Query query={ALL_ITEMS_QUERY}>
+        <Query
+          query={ALL_ITEMS_QUERY}
+          // fetchPolicy="network-only"
+          variables={{
+            skip: this.props.page * perPage - perPage
+          }}
+        >
           {({ data, error, loading }) => {
             if (loading) return <p>LOADING...</p>;
             if (error) return <p>Error: {error.message}</p>;
             return (
-              <ItemsList>
-                {data.items.map(item => (
-                  <Item item={item} key={item.id} />
-                ))}
-              </ItemsList>
+              <>
+                <Pagination page={this.props.page} />
+                <ItemsList>
+                  {data.items.map(item => (
+                    <Item item={item} key={item.id} />
+                  ))}
+                </ItemsList>
+                <Pagination page={this.props.page} />
+              </>
             );
           }}
         </Query>
-        <Pagination page={this.props.page} />
       </Center>
     );
   }
