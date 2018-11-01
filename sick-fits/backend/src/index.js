8938e35d36de699423b100ceeb3f4c03c1f59251
Rob Eberhardt <rob.eberhardt@gmail.com>
2018-10-31 23:50:11 -0400

    end of chapter 26

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,12 +1,22 @@
 const cookieParser = require("cookie-parser");
 require("dotenv").config({ path: "variables.env" });
 const createServer = require("./createServer");
+const jwt = require("jsonwebtoken");
 const db = require("./db");
 
 const server = createServer();
 server.express.use(cookieParser());
 
-// TODO use express middleware to populate current user
+// decode the JWT so we can get the user Id on each request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    // put the userId onto the request for future requests
+    req.userId = userId;
+  }
+  next();
+});
 
 server.start(
   {
